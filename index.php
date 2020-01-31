1ed5807ae52903d1aec8710589ac3c3af7ca63a0
LutenkoMikhail <lamprologus2011@gmail.com>
2020-01-31 15:53:32 +0200

    Fix bag and refactoring

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php
 require_once '.\vendor\autoload.php';
-use src\Cat;
-use src\Wolf;
-use src\Artiodactyls;
-use src\AquaticOrganism;
-use src\Insect;
+
+use src\Cat, src\Wolf, src\Artiodactyls, src\AquaticOrganism, src\Insect;
 
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
@@ -16,33 +13,38 @@ echo "<h3>Test task. </h3>";
 echo "<h3>3.2 Zoo </h3>";
 
 $lion = new Cat('Lion');
-echo $lion.PHP_EOL;
-echo 'Voice : '.$lion->makeSound().PHP_EOL;
+echo $lion . PHP_EOL;
+echo 'Voice : ' . $lion->makeSound() . PHP_EOL;
 
 $cougar = new Cat('Cougar');
-echo $cougar.PHP_EOL;
-echo 'Voice : '.$cougar->makeSound().PHP_EOL;
+echo $cougar . PHP_EOL;
+echo 'Voice : ' . $cougar->makeSound() . PHP_EOL;
 
 $wolf = new Wolf('Wolf');
 $wolf->setWaysFood('Makes food itself');
-echo $wolf.PHP_EOL;
+echo $wolf . PHP_EOL;
 
 $dog = new Wolf('Dog');
 $dog->setWaysFood('Asks for food from a person');
 $dog->setVote('gavvvvv....');
-echo $dog.PHP_EOL;
+echo $dog . PHP_EOL;
 
 $antilopa = new \src\Artiodactyls('Antilopa', 'biy.....');
-echo $antilopa.PHP_EOL;
+echo $antilopa . PHP_EOL;
 
 $elephant = new \src\Artiodactyls('Elephant', 'yyyy.....');
-echo $elephant.PHP_EOL;
+echo $elephant . PHP_EOL;
 
 $trout = new \src\AquaticOrganism('Fresh water', 'Trout');
-echo $trout.PHP_EOL;
+echo $trout . PHP_EOL;
 
 $shark = new \src\AquaticOrganism('salt water', 'Shark');
-echo $shark.PHP_EOL;
+echo $shark . PHP_EOL;
 
 $insect = new \src\Insect();
-echo $insect.PHP_EOL;
+echo $insect . PHP_EOL;
+
+/* T.E comments:
+ * Just tip: In one namespace you can list class names with a comma. Only in PHP7+ update.
+ */
+
